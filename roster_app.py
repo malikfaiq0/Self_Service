e084aa9bacb8f479d68e3e07ba9aafe952bd474a
malikfaiq0 <88503432+malikfaiq0@users.noreply.github.com>
2025-04-04 08:53:51 +0500

    Add files via upload

--- roster_app.py
+++ roster_app.py
@@ -21,18 +21,15 @@ def load_css():
 load_css()
 
 # Database connection function
-DB_SERVER = "39.34.150.71"
-DB_NAME = "RosterManagement"
-DB_USERNAME = "myuser"
-DB_PASSWORD = "!Mynameisapp"
 
 def get_db_connection():
     conn = pyodbc.connect(
-        f"DRIVER={{ODBC Driver 17 for SQL Server}};"
-        f"SERVER={DB_SERVER},1433;"
-        f"DATABASE={DB_NAME};"
-        f"UID={DB_USERNAME};"
-        f"PWD={DB_PASSWORD};"
+        "DRIVER={ODBC Driver 17 for SQL Server};"
+        "SERVER=39.34.150.71;"
+        "PORT=1433;"
+        "DATABASE=RosterManagement;"
+        "UID=myuser;"
+        "PWD=!Mynameisapp;"
     )
     return conn
 # Test connection
